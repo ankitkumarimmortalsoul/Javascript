f2a7262590eea521e1401145b480bb651a3e1a7a
ankitkumarimmortalsoul <107613085+ankitkumarimmortalsoul@users.noreply.github.com>
2024-09-16 08:26:06 +0000

    few methods

---
+++ basics/Strings-practice.js
@@ -0,0 +1,41 @@
+// let var1=1;
+// let var2=1234567890n;
+// let var3="a";
+// let var4 = {
+//     name:"aman",
+//     lastName:"kumar"
+// }
+// let var5 = null;
+// let var6;
+
+// console.log(typeof var1);
+// console.log(typeof var2);
+// console.log(typeof var3);
+// console.log(typeof var4);
+// console.log(typeof var5);
+// console.log(typeof var6);
+
+
+
+// Strings in JS
+let firstName = "aman" ;
+let lastName = "kumar" ;
+const city = "jaipur" ;
+//const city = new String("jaipur") ;
+console.log(city);
+//console.log(`his name is ${firstName} ${lastName}`);
+let fullName = firstName + " " + lastName;
+// caonsole.log(`his name is ${fullName}`);
+// console.log(`city name starts by ${city[0]}`);
+// console.log(`person name: ${lastName[0]} ${firstName[0]} lives in ${city[0]}`);
+// console.log(fullName.__proto__);
+// console.log(fullName.charAt(0));
+// console.log(fullName.indexOf(" "));
+// console.log(fullName.charAt(fullName.indexOf(" ")+1));
+// console.log(fullName.toUpperCase());
+// fullName= firstName.toUpperCase() + " "+ lastName.toUpperCase();
+// console.log(fullName);
+// city = "Kanpur" ;
+let newVisitor = "               LOKESH             " ;
+console.log(newVisitor);
+console.log(newVisitor.trim());
