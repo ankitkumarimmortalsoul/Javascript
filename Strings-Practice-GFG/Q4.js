07a514ba9b6e2e5c894fe92223be3af9b38f003d
ankitkumarimmortalsoul <107613085+ankitkumarimmortalsoul@users.noreply.github.com>
2024-09-25 09:17:25 +0000

    completed

---
+++ Strings-Practice-GFG/Q4.js
@@ -0,0 +1,5 @@
+// program to remove a character from  string in JavaScript.
+const siteDomainName = "Geeks for Geeks" ;
+const editText = "geeks"
+console.log(siteDomainName.replace("Geeks" , editText));
+console.log(siteDomainName);
