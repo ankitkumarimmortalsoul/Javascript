82b343b6c667fdde19c37c6908f7f65e158ade8c
ankitkumarimmortalsoul <107613085+ankitkumarimmortalsoul@users.noreply.github.com>
2024-09-26 06:02:55 +0000

    complete

---
+++ Strings-Practice-GFG/Q14.js
@@ -0,0 +1,8 @@
+let testString1 = "Ankit" ;
+let testString2 = "K." ;
+const finalString = testString1 + " " + testString2 ; 
+console.log(finalString);
+console.log(testString1.concat(" " , testString2));
+console.log(testString2.concat(" " , testString1));
+// template literals is back tick ````````
+console.log(`hi i am ${testString1} ${testString2} studying here in ${testString2} ${testString1} institute of technology`);
