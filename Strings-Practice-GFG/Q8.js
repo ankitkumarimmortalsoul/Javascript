e93ce9dd8e85264dea8f49e4b24976cf8d89ebbc
ankitkumarimmortalsoul <107613085+ankitkumarimmortalsoul@users.noreply.github.com>
2024-09-26 05:20:20 +0000

    completed

---
+++ Strings-Practice-GFG/Q8.js
@@ -0,0 +1,3 @@
+const testString = "geeks for geeks" ;
+console.log(testString.charAt(0).toUpperCase() + testString.slice(1));
+console.log(testString.replace(testString[0] , testString[0].toUpperCase()));
